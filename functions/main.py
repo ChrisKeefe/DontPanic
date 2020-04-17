15121a9a9da92d618c473d8fa06671a2f2d39fa7
Chris Keefe <crk239@nau.edu>
2020-04-16 17:27:31 -0700

    adds python-runtime files to GCF functions dir

---
+++ functions/main.py
@@ -0,0 +1,73 @@
+from google.cloud import firestore
+from flask import escape
+
+
+def hello_world(request):
+    """Responds to any HTTP request.
+    Args:
+        request (flask.Request): HTTP request object.
+    Returns:
+        The response text or any set of values that can be turned into a
+        Response object using
+        `make_response <http://flask.pocoo.org/docs/1.0/api/#flask.Flask.make_response>`.
+    Usage: passing a message as follows will display that message
+    https://GCP_REGION-PROJECT_ID.cloudfunctions.net/hello_http?message=<some message text>
+    """
+    request_json = request.get_json()
+    if request.args and 'message' in request.args:
+        return request.args.get('message')
+    elif request_json and 'message' in request_json:
+        return request_json['message']
+    else:
+        return f'Hello World!'
+
+
+def hello_http(request):
+    """HTTP Cloud Function.
+    Args:
+        request (flask.Request): The request object.
+        <http://flask.pocoo.org/docs/1.0/api/#flask.Request>
+    Returns:
+        The response text, or any set of values that can be turned into a
+        Response object using `make_response`
+        <http://flask.pocoo.org/docs/1.0/api/#flask.Flask.make_response>.
+    Usage: passing a name as follows will yield Hello Name!
+    https://GCP_REGION-PROJECT_ID.cloudfunctions.net/hello_http?name=NAME
+    """
+    request_json = request.get_json(silent=True)
+    request_args = request.args
+
+    if request_json and 'name' in request_json:
+        name = request_json['name']
+    elif request_args and 'name' in request_args:
+        name = request_args['name']
+    else:
+        name = 'World'
+    return 'Hello {}!'.format(escape(name))
+
+# we can use publishers and subscribers to chain multiple cloud functions
+# e.g. a function runs then publishes to a channel monitored by other functions
+# which run in turn
+# see: https://cloud.google.com/functions/docs/calling/pubsub
+# and: https://cloud.google.com/functions/docs/tutorials/pubsub
+
+def hello_pubsub(event, context):
+    """Background Cloud Function to be triggered by Pub/Sub.
+    Args:
+         event (dict):  The dictionary with data specific to this type of
+         event. The `data` field contains the PubsubMessage message. The
+         `attributes` field will contain custom attributes if there are any.
+         context (google.cloud.functions.Context): The Cloud Functions event
+         metadata. The `event_id` field contains the Pub/Sub message ID. The
+         `timestamp` field contains the publish time.
+    """
+    import base64
+
+    print("""This Function was triggered by messageId {} published at {}
+    """.format(context.event_id, context.timestamp))
+
+    if 'data' in event:
+        name = base64.b64decode(event['data']).decode('utf-8')
+    else:
+        name = 'World'
+    print('Hello {}!'.format(name))
