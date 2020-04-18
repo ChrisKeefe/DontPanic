06bf3cd53bb0b0da0724eac806b1db9828418026
Chris Keefe <crk239@nau.edu>
2020-04-17 21:32:05 -0700

    adds create_profile and create_message functions

--- functions/main.py
+++ functions/main.py
@@ -75,20 +75,120 @@ def hello_pubsub(event, context):
     print('Hello {}!'.format(name))
 
 
-# ################### Begin actual Don't Panic code ############################
+# ################### Begin actual Don't Panic code ###########################
 
-
-def add_from_dict(request):
+def create_message(request):
+    """ Adds a new Message document to the messages collection in firestore
+    Preconditions: receives a POST request of content-type 'application/json'
+        format:
+        {
+          "messagePayload":{
+              "messageContent":"STR",
+              "resourceURL":"STR",
+              "category":"STR",
+              "urgency":"INT",
+              "tags":[
+              "STR1",
+              "STR2",
+              "STRn"
+              ]
+            }
+          }
+    Postconditions: uniquely identified db document written to firestore
+    """
+    import uuid
     db = firestore.Client()
+    # TODO: sanitize inputs
+
+    # get_json gets a json object if exists, else returns None
+    request_json = request.get_json(silent=True)
+    # args is a multidict, which can be indexed like request_args[key]
+    request_args = request.args
+    if request_json and 'messagePayload' in request_json:
+        payload = request_json['messagePayload']
+    elif request_args and 'messagePayload' in request_args:
+        payload = request_args['messagePayload']
+
+    messageContent = payload['messageContent']
+    resourceURL = payload['resourceURL']
+    category = payload['category']
+    urgency = payload['urgency']
+    tags = payload['tags']
+
     data = {
-        u'name': u'Los Angeles',
-        u'state': u'CA',
-        u'country': u'USA'
+        u'messageContent': str(messageContent),
+        u'resourceURL': str(resourceURL),
+        u'category': str(category),
+        u'tags': tags,
+        u'urgency': int(urgency)
     }
 
-    # Add a new doc in collection 'cities' with ID 'LA'
-    db.collection(u'cities').document(u'LA').set(data)
+    # generate a unique id for db documents to prevent namespace collisions
+    message_ID = uuid.uuid4()
+    # Add a new doc in collection 'messages' with randomly-generated ID
+    db.collection(u'messages').document(str(message_ID)).set(data)
 
+
+def create_profile(request):
+    """ Adds a new Profile document to the profiles collection in firestore
+    Preconditions: Must have a user ID and other profile data delivered by as a 
+    POST request of content-type 'application/json', with format:
+        {
+          "profilePayload":{
+              "userID":"INT",
+              "receiveSMSflag":"BOOL",
+              "receivePushFlag":"BOOL",
+              "messageFrequency":"INT",
+              "isProfileActive":"BOOL",
+              "preferredTags":[
+              "STR1",
+              "STR2",
+              "STRn"
+              ]
+            }
+          }
+    Postconditions: uniquely identified db document written to firestore
+    TODO: add a message scheduler to this
+    TODO: sanitize inputs
+    """
+    import uuid
+    db = firestore.Client()
+
+    # get_json gets a json object if exists, else returns None
+    request_json = request.get_json(silent=True)
+    # args is a multidict, which can be indexed like request_args[key]
+    request_args = request.args
+    if request_json and 'profilePayload' in request_json:
+        payload = request_json['profilePayload']
+    elif request_args and 'profilePayload' in request_args:
+        payload = request_args['profilePayload']
+
+    userID = payload['userID']
+    receiveSMSflag = payload['receiveSMSflag']
+    receivePushFlag = payload['receivePushFlag']
+    messageFrequency = payload['messageFrequency']
+    isProfileActive = payload['isProfileActive']
+    preferredTags = payload['preferredTags']
+
+    data = {
+        u'userID': int(userID),
+        u'receiveSMSflag': bool(receiveSMSflag),
+        u'receivePushFlag': bool(receivePushFlag),
+        u'messageFrequency': int(messageFrequency),
+        u'isProfileActive': bool(isProfileActive),
+        u'preferredTags': preferredTags
+    }
+
+    # generate a unique id for db documents to prevent namespace collisions
+    profile_ID = uuid.uuid4()
+    # Add a new doc in collection 'messages' with randomly-generated ID
+    db.collection(u'profiles').document(str(profile_ID)).set(data)
+
+
+# ############################################################################
+# TODO: Updating messages or profiles will require us to get the record,
+# get existing tags, set() those tags, add the new ones, and then create the
+# record passing the recordID explicitly
 
 # TODO: Mason - create a function that sends a simple text message to our app
 
