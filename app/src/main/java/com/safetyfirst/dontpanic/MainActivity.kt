55e300713f662ab2fdf7ff247ec82fe85f97e9fb
Chris Keefe <crk239@nau.edu>
2020-04-18 18:00:26 -0700

    ROUGH: FCM/Android install tweak, Reports DeviceID

--- app/src/main/java/com/safetyfirst/dontpanic/MainActivity.kt
+++ app/src/main/java/com/safetyfirst/dontpanic/MainActivity.kt
@@ -1,14 +1,16 @@
 package com.safetyfirst.dontpanic
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.iid.FirebaseInstanceId
-
+import com.safetyfirst.dontpanic.MyFirebaseMessagingService
 
 
 class MainActivity : AppCompatActivity() {
+    private val TAG = "MyFirebaseMsgService"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,8 +29,25 @@ class MainActivity : AppCompatActivity() {
                 val msg =  token
 
                 //UnComment out if you need the token ID for testing
-                //Toast.makeText(baseContext, msg, Toast.LENGTH_LONG).show()
+//                TODO: Comment out in production code
+                Toast.makeText(baseContext, msg, Toast.LENGTH_LONG).show()
             })
+
+//        FirebaseInstanceId.getInstance().instanceId
+//            .addOnCompleteListener(OnCompleteListener { task ->
+//                if (!task.isSuccessful) {
+//                    Log.w(TAG, "getInstanceId failed", task.exception)
+//                    return@OnCompleteListener
+//                }
+//
+//                // Get new Instance ID token
+//                val token = task.result?.token
+//
+//                // Log and toast
+//                val msg = getString(R.string.msg_token_fmt, token)
+//                Log.d(TAG, msg)
+//                Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
+//            })
 
     }
 
--- app/src/main/java/com/safetyfirst/dontpanic/MyFirebaseMessagingService.java
+++ app/src/main/java/com/safetyfirst/dontpanic/MyFirebaseMessagingService.java
@@ -24,6 +24,26 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
         if (remoteMessage.getNotification() != null) {
             Log.d(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
         }
-
     }
+
+    /**
+     * Called if InstanceID token is updated. This may occur if the security of
+     * the previous token had been compromised. Note that this is called when the InstanceID token
+     * is initially generated so this is where you would retrieve the token.
+     */
+    @Override
+    public void onNewToken(String token) {
+        Log.d(TAG, "Refreshed token: " + token);
+
+        // If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // Instance ID token to your app server.
+        sendRegistrationToServer(token);
+    }
+
+    public void sendRegistrationToServer(String token) {
+        Log.d(TAG, token);
+//        TODO Implement
+    }
+
 }
